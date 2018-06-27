a414d5f1ed382349a006c1c15de7c1e6af7e35c8
Osama bin bashir <osama.binbashir@yahoo.com>
2018-06-27 17:17:45 +0500

    FInal commit

---
+++ Halaat Updates/Halaat Updates/Network/Services/Endpoint.swift
@@ -0,0 +1,13 @@
+//
+//  Endpoint.swift
+//  Halaat Updates
+//
+//  Created by inVenD on 24/06/2018.
+//  Copyright © 2018 freelance. All rights reserved.
+//
+
+import Foundation
+
+protocol Endpoint{
+    
+}
